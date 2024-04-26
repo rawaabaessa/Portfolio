1c51ad45018cfa22e16e95c9df9d6a07105f67f0
rawaabaessa <rawaabaessa@gmail.com>
2024-04-26 18:54:07 +0300

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ import "animate.css";
 import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
-  let CVFileURL = "http://localhost:3000/CV.pdf";
+  let CVFileURL = "http://rawaaba.netlify.app/CV.pdf";
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
